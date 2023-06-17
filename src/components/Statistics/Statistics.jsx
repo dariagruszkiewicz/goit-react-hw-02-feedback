1636a46c0c9d96b0381b4aa0c338f66b3ff3b5ca
Daria Gruszkiewicz <dariaa.pajak@gmail.com>
2023-06-17 22:32:02 +0200

    update
    dodanie prop-types

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,4 +1,5 @@
 import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
 
 export const Statistics = ({
   good,
@@ -16,4 +17,12 @@ export const Statistics = ({
       <li>Positive feedback: {positivePercentage}%</li>
     </ul>
   );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+  positivePercentage: PropTypes.string,
 };
